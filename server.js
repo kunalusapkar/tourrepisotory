346be39a4efe1036e78c80a00c03911279edeb41
kunalusapkar <kusapkar@gmail.com>
2019-12-09 14:44:08 +0530

    Small changes

--- server.js
+++ server.js
@@ -2,30 +2,47 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
-    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
     console.log(err.name, err.message);
     process.exit(1);
-});
+
+})
 
 dotenv.config({
     path: './config.env'
 });
 const app = require('./app');
-
-const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-);
-
-mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    })
-    .then(() => console.log('DB connection successful!'));
-
 const port = process.env.PORT || 3000;
+
+
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => console.log("Connection succesfull"));
+
+// const testTour = new Tour({
+//     name: "Gujrat National Park",
+//     ratings: 4.5,
+//     price: 470
+// });
+
+// testTour.save().then(doc => {
+//     console.log(doc);
+// }).catch(err => {
+//     console.log("Error", err);
+// })
+
 const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+    console.log('Server is launching');
+});
+// To handle promise rejection
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
+
 });
